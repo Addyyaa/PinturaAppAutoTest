c89472ca71c1709e2cfdaccd1253e118581f749f
addyya <2698567570@qq.com>
2025-01-16 16:42:29 +0800

    完成登录

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -28,12 +28,67 @@ class LoginPage extends Page {
     }
 
     public get initailRegionSearchIcon() {
-        return $('//android.widget.TextView[@text=""]');
+        return $('//android.view.View[@resource-id="easy-input"]');
+    }
+
+    public get initailLanguageSelect() {
+        return $('//android.webkit.WebView[@text="pages/area[2]"]/android.view.View[2]/android.view.View');
+    }
+
+    public get initailLanguageEn() {
+        return $('//android.widget.TextView[@text="English"]');
+    }
+
+    public get initailLanguageZh() {
+        return $('(//android.widget.TextView[@text="简体中文"])[2]');
+    }
+
+    public get initailFinalConfirmButtonEn() {
+        return $('//android.widget.TextView[@text="Confirm"]');
+    }
+
+    public get initailFinalConfirmButtonZh() {
+        return $('//android.widget.TextView[@text="确定"]');
     }
 
     public get accountInput() {
-        return $('selector');
+        return $('(//android.widget.EditText)[1]');
     }
+
+    public get passwordInput() {
+        return $('(//android.widget.EditText)[2]');
+    }
+
+    public get loginButton() {
+        return $('//android.widget.TextView[@text="登录" or @text="Login"]');
+    }
+
+    public get loginPravicyToast() {
+        return $('//android.widget.TextView[@text="同意并登录" or @text="Agree and Log In"]');
+    }
+
+    public get loginPravicyToastCancel() {
+        return $('//android.widget.TextView[@text="取消" or @text="Cancel"]');
+    }
+    
+    public get loginWithEmailButton() {
+        return $('//android.widget.TextView[@text="邮箱登录" or @text="E-mail"]');
+    }
+
+    public get loginWithPhoneButton() {
+        return $('//android.widget.TextView[@text="手机登录" or @text="Phone"]');
+    }
+
+    public get loginWithCodeButton() {
+        return $('//android.widget.TextView[@text="验证码登录" or @text="Code"]');
+    }
+
+    public get skipButton1zh() {
+        return $('//android.widget.TextView[@text="Skip" or @text="下一步"]')
+    }
+    
+    
+    
 
 
 }
--- test/specs/test.login.ts
+++ test/specs/test.login.ts
@@ -1,8 +1,5 @@
-import { expect } from '@wdio/globals';
 import LoginPage from '../../pages/login.page';
-import { Key } from 'webdriverio'
 import * as testData from '../data/test-data.json' assert { type: 'json' };
-import { execute } from 'webdriverio/build/commands/browser';
 
 describe('登录模块测试', () => {
     let expectkey: string;
@@ -17,8 +14,9 @@ describe('登录模块测试', () => {
     describe('登录测试', () => {
         it('初始化语言和地区', async () => {
             const button = LoginPage.inintalPravicyAgreeButton;
+            await browser.pause(700)
             await button.waitForDisplayed({ timeout: 5000 })
-            await browser.pause(1000)
+            await button.getAttribute('clickable') === 'true'
             await button.click();
             await LoginPage.initalRegionButton.click()
             const { width, height } = await browser.getWindowSize();
@@ -27,14 +25,49 @@ describe('登录模块测试', () => {
             const searchIcon = LoginPage.initailRegionSearchIcon
             await searchIcon.waitForDisplayed({ timeout: 5000 })
             await browser.actions([
-                browser.action('pointer').move(width * 0.463, height * 0.3588)
+                browser.action('pointer').move(width * 0.463, height * 0.3588) // 只能通过坐标点击选择语言，使用元素高概率性出现选择了中文结果切换成了英文模式
                 .down()
                 .up()
             ])
             await browser.keys(testData.login.case1.region)
             const regionText = $(`//android.widget.TextView[contains(@text, '${testData.login.case1[expectkey as keyof typeof testData.login.case1]}')]`)
             await regionText.waitForDisplayed({ timeout: 5000 })
-            await regionText.click()
+            await regionText.click() // 选择地区
+            await LoginPage.initailLanguageSelect.click() // 选择语言
+            const languages = expectkey === 'expect' ? LoginPage.initailLanguageZh : LoginPage.initailLanguageEn
+            console.log('languages====',expectkey)
+            const text = await languages.getText()
+            console.log('text====',text)
+            await browser.actions([
+                browser.action('pointer').move(width * 0.501, height * 0.87333)
+                .down()
+                .up()
+            ])
+            const finalConfirmButton = expectkey === 'expect' ? LoginPage.initailFinalConfirmButtonZh : LoginPage.initailFinalConfirmButtonEn
+            await finalConfirmButton.click()
+        })
+
+        it('正常登录-case2', async () => {
+            if (testData.login.case2.account.includes('@')) {
+                const loginWithEmailButton = LoginPage.loginWithEmailButton
+                await loginWithEmailButton.waitForDisplayed({ timeout: 5000 })
+                await loginWithEmailButton.click()
+            } else {
+                const loginWithPhoneButton = LoginPage.loginWithPhoneButton
+                await loginWithPhoneButton.waitForDisplayed({ timeout: 5000 })
+                await loginWithPhoneButton.click()
+            }
+            const accountInput = LoginPage.accountInput
+            await accountInput.waitForDisplayed({ timeout: 5000 })
+            await accountInput.addValue(testData.login.case2.account)
+            const passwordInput = LoginPage.passwordInput
+            await passwordInput.waitForDisplayed({ timeout: 5000 })
+            await passwordInput.click()
+            await passwordInput.addValue(testData.login.case2.password)
+            await LoginPage.loginButton.click()
+            const loginPravicyToast = LoginPage.loginPravicyToast
+            await loginPravicyToast.click()
+            await LoginPage.skipButton1zh.isExisting()
         })
         
     })
